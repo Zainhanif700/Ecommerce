a512f438ba313280fb17a20b95c1df3ae6574e34
Kazim Jawad <67737501+kazimJawad@users.noreply.github.com>
2025-02-07 15:54:51 +0100

    Update Order.jsx

--- src/customer/components/Order/Order.jsx
+++ src/customer/components/Order/Order.jsx
@@ -1,28 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import OrderCard from "./OrderCard";
 import Grid from "@mui/material/Grid";
+import CircularProgress from "@mui/material/CircularProgress";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { getCurrentOrder } from '../../../State/Order/Action.js';
+import { getCurrentOrder } from "../../../State/Order/Action.js";
 
 function Order() {
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const { order } = useSelector((store) => store);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        dispatch(getCurrentOrder())
-    }, [])
+        setLoading(true); // Show loading state
+        dispatch(getCurrentOrder()).then(() => setLoading(false)); // Hide when data is fetched
+    }, [dispatch]);
 
     return (
         <div className="px-5 lg:px-20">
-            <Grid item xs={9}>
-                <div className="space-y-5">
-                    {order?.orders?.map((order) => <OrderCard order={order}/>)}
+            {loading ? (
+                <div className="flex justify-center items-center h-[50vh]">
+                    <CircularProgress />
                 </div>
-            </Grid>
+            ) : (
+                <Grid item xs={9}>
+                    <div className="space-y-5">
+                        {order?.orders?.length > 0 ? (
+                            order.orders.map((order) => <OrderCard key={order.id} order={order} />)
+                        ) : (
+                            <p className="text-center text-xl">No Orders Found</p>
+                        )}
+                    </div>
+                </Grid>
+            )}
         </div>
-    )
+    );
 }
 
 export default Order;
