b9761f787796e131a034ed2905c550977962722d
Kazim Jawad <67737501+kazimJawad@users.noreply.github.com>
2025-02-07 16:00:44 +0100

    Update Navigation.jsx

--- src/customer/components/Navigation/Navigation.jsx
+++ src/customer/components/Navigation/Navigation.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Fragment, useEffect, useState } from 'react'
+import { Fragment, useEffect, useState } from 'react';
 import {
   Dialog,
   DialogBackdrop,
@@ -12,87 +12,24 @@ import {
   TabList,
   TabPanel,
   TabPanels,
-} from '@headlessui/react'
+} from '@headlessui/react';
 
-import { Bars3Icon, ShoppingBagIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import Avatar from '@mui/material/Avatar'
-import { deepPurple } from '@mui/material/colors'
-import Button from '@mui/material/Button'
-import Menu from '@mui/material/Menu'
-import MenuItem from '@mui/material/MenuItem'
-import { useLocation, useNavigate } from 'react-router-dom'
-import AuthModal from '../../Auth/AuthModel.jsx'
-import { useDispatch, useSelector } from 'react-redux'
-import { getUser } from "../../../State/Auth/Action.js";
-import { logout } from '../../../State/Auth/Action.js';
-import { toast } from 'react-toastify'
-
-const navigation = {
-  categories: [
-    {
-      id: 'search furniture',
-      name: 'Search by Category',
-      featured: [
-        {
-          name: 'New Arrivals',
-          href: '#',
-          imageSrc: 'https://tailwindui.com/plus/img/ecommerce-images/product-page-04-detail-product-shot-01.jpg',
-          imageAlt: 'Drawstring top with elastic loop closure and textured interior padding.',
-        },
-        {
-          name: 'Artwork Tees',
-          href: '#',
-          imageSrc: 'https://tailwindui.com/plus/img/ecommerce-images/category-page-02-image-card-06.jpg',
-          imageAlt:
-            'Three shirts in gray, white, and blue arranged on table with same line drawing of hands and shapes overlapping on front of shirt.',
-        },
-      ],
-      sections: [
-        {
-          id: 'furniture',
-          name: 'Furniture',
-          items: [
-            { name: 'Sofas', href: '#' },
-            { name: 'Chairs', href: '#' },
-            { name: 'Tables', href: '#' },
-            { name: 'Beds', href: '#' },
-            { name: 'Cabinets', href: '#' },
-            { name: 'Outdoor Furniture', href: '#' },
-          ],
-        },
-        {
-          id: 'decor',
-          name: 'Decoration',
-          items: [
-            { name: 'Rugs', href: '#' },
-            { name: 'Lamps', href: '#' },
-            { name: 'Wall Art', href: '#' },
-            { name: 'Mirrors', href: '#' },
-            { name: 'Cushions', href: '#' },
-            { name: 'Curtains', href: '#' },
-          ],
-        },
-        {
-          id: 'brands',
-          name: 'Brands',
-          items: [
-            { name: 'Modern Home', href: '#' },
-            { name: 'Rustic Charm', href: '#' },
-            { name: 'Urban Living', href: '#' },
-            { name: 'EcoStyle', href: '#' },
-          ],
-        },
-      ],
-
-    },
-  ],
-  pages: [
-  ],
-}
+import { Bars3Icon, ShoppingBagIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import Avatar from '@mui/material/Avatar';
+import { deepPurple } from '@mui/material/colors';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import CircularProgress from '@mui/material/CircularProgress';
+import { useLocation, useNavigate } from 'react-router-dom';
+import AuthModal from '../../Auth/AuthModel.jsx';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUser, logout } from "../../../State/Auth/Action.js";
 
 export default function Example() {
   /* React State */
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(true); // Loading state
   const jwt = localStorage?.getItem("jwt");
   const { auth } = useSelector((store) => store);
   const [anchorEl, setAnchorEl] = useState(null);
@@ -103,10 +40,6 @@ export default function Example() {
   const location = useLocation();
 
   /* React Functions */
-  function classNames(...classes) {
-    return classes.filter(Boolean).join(" ");
-  }
-
   const handleUserClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
@@ -128,128 +61,16 @@ export default function Example() {
     handleCloseUserMenu();
   };
 
-  const handleCategoryClick = (category, section, item) => {
-    navigate(`/${category.id}/${section.id}/${item.name}`);
-  };
-
   useEffect(() => {
-    if (jwt)
-      dispatch(getUser(jwt));
-  }, [jwt, auth?.jwt])
-
-  useEffect(() => {
-    if (auth?.user != null)
-      handleClose();
-
-
-  }, [auth?.user])
+    if (jwt) {
+      dispatch(getUser(jwt)).then(() => setLoading(false));
+    } else {
+      setLoading(false);
+    }
+  }, [jwt, dispatch]);
 
   return (
     <div className="bg-white pb-10">
-      {/* Mobile menu */}
-      <Dialog open={open} onClose={setOpen} className="relative z-40 lg:hidden">
-        <DialogBackdrop
-          transition
-          className="fixed inset-0 bg-black/25 transition-opacity duration-300 ease-linear data-[closed]:opacity-0"
-        />
-
-        <div className="fixed inset-0 z-40 flex">
-          <DialogPanel
-            transition
-            className="relative flex w-full max-w-xs transform flex-col overflow-y-auto bg-white pb-12 shadow-xl transition duration-300 ease-in-out data-[closed]:-translate-x-full"
-          >
-            <div className="flex px-4 pb-2 pt-5">
-              <button
-                type="button"
-                onClick={() => setOpen(false)}
-                className="relative -m-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400"
-              >
-                <span className="absolute -inset-0.5" />
-                <span className="sr-only">Close menu</span>
-                <XMarkIcon aria-hidden="true" className="size-6" />
-              </button>
-            </div>
-
-            {/* Links */}
-            <TabGroup className="mt-2">
-              <div className="border-b border-gray-200">
-                <TabList className="-mb-px flex space-x-8 px-4">
-                  {navigation.categories.map((category) => (
-                    <Tab
-                      key={category.name}
-                      className="flex-1 whitespace-nowrap border-b-2 border-transparent px-1 py-4 text-base font-medium text-gray-900 data-[selected]:border-indigo-600 data-[selected]:text-indigo-600"
-                    >
-                      {category.name}
-                    </Tab>
-                  ))}
-                </TabList>
-              </div>
-              <TabPanels as={Fragment}>
-                {navigation.categories.map((category) => (
-                  <TabPanel key={category.name} className="space-y-10 px-4 pb-8 pt-10">
-                    <div className="grid grid-cols-2 gap-x-4">
-                      {category.featured.map((item) => (
-                        <div key={item.name} className="group relative text-sm">
-                          <img
-                            alt={item.imageAlt}
-                            src={item.imageSrc}
-                            className="aspect-square w-full rounded-lg bg-gray-100 object-cover group-hover:opacity-75"
-                          />
-                          <a href={item.href} className="mt-6 block font-medium text-gray-900">
-                            <span aria-hidden="true" className="absolute inset-0 z-10" />
-                            {item.name}
-                          </a>
-                          <p aria-hidden="true" className="mt-1">
-                            Shop now
-                          </p>
-                        </div>
-                      ))}
-                    </div>
-                    {category.sections.map((section) => (
-                      <div key={section.name}>
-                        <p id={`${category.id}-${section.id}-heading-mobile`} className="font-medium text-gray-900">
-                          {section.name}
-                        </p>
-                        <ul
-                          role="list"
-                          aria-labelledby={`${category.id}-${section.id}-heading-mobile`}
-                          className="mt-6 flex flex-col space-y-6"
-                        >
-                          {section.items.map((item) => (
-                            <li key={item.name} className="flow-root">
-                              <p
-                                onClick={() => {
-                                  handleCategoryClick(category, section, item);
-                                  close(); 
-                                }}
-                                className="cursor-pointer hover:text-gray-800"
-                              >
-                                {item.name}
-                              </p>
-                            </li>
-                          ))}
-                        </ul>
-                      </div>
-                    ))}
-                  </TabPanel>
-                ))}
-              </TabPanels>
-            </TabGroup>
-
-            <div className="space-y-6 border-t border-gray-200 px-4 py-6">
-              {navigation.pages.map((page) => (
-                <div key={page.name} className="flow-root">
-                  <a href={page.href} className="-m-2 block p-2 font-medium text-gray-900">
-                    {page.name}
-                  </a>
-                </div>
-              ))}
-            </div>
-
-          </DialogPanel>
-        </div>
-      </Dialog>
-
       <header className="relative bg-white">
         <p className="flex h-10 items-center justify-center bg-[#563232] px-4 text-sm font-medium text-white sm:px-6 lg:px-8">
           LowTech GmbH
@@ -263,151 +84,34 @@ export default function Example() {
                 onClick={() => setOpen(true)}
                 className="rounded-md bg-white p-2 text-gray-400 lg:hidden"
               >
-                <span className="sr-only">Open menu</span>
                 <Bars3Icon aria-hidden="true" className="h-6 w-6" />
               </button>
 
               {/* Logo */}
               <div className="ml-4 flex lg:ml-0">
                 <a href="/">
-                  <span className="sr-only">Company</span>
                   <img
-                    alt=""
+                    alt="Company Logo"
                     src="https://lowtechgmbhstorage.blob.core.windows.net/lowtechgmbh-blob/Screenshot 2025-02-06 193950.png"
                     className="h-8 w-auto"
                   />
                 </a>
               </div>
 
-              {/* Flyout menus */}
-              <PopoverGroup className="hidden lg:ml-8 lg:block lg:self-stretch z-10">
-                <div className="flex h-full space-x-8">
-                  {navigation.categories.map((category) => (
-                    <Popover key={category.name} className="flex">
-                      {({ open, close }) => ( // Render-prop pattern to access 'close'
-                        <>
-                          <div className="relative flex">
-                            <Popover.Button
-                              className={classNames(
-                                open
-                                  ? "border-indigo-600 text-indigo-600"
-                                  : "border-transparent text-gray-700 hover:text-gray-800",
-                                "relative z-10 -mb-px flex items-center border-b-2 pt-px text-sm font-medium text-gray-700 transition-colors duration-200 ease-out hover:text-gray-800"
-                              )}
-                            >
-                              {category.name}
-                            </Popover.Button>
-                          </div>
-
-                          <Popover.Panel
-                            transition
-                            className="absolute inset-x-0 top-full text-sm text-gray-500"
-                            style={{ zIndex: 10 }}
-                          >
-                            <div
-                              aria-hidden="true"
-                              className="absolute inset-0 top-1/2 bg-white shadow"
-                            />
-
-                            <div className="relative bg-white">
-                              <div className="mx-auto max-w-7xl px-8">
-                                <div className="grid grid-cols-2 gap-x-8 gap-y-10 py-16">
-                                  <div className="col-start-2 grid grid-cols-2 gap-x-8">
-                                    {category.featured.map((item) => (
-                                      <div
-                                        key={item.name}
-                                        className="group relative text-base sm:text-sm"
-                                      >
-                                        <img
-                                          alt={item.imageAlt}
-                                          src={item.imageSrc}
-                                          className="aspect-square w-full rounded-lg bg-gray-100 object-cover group-hover:opacity-75"
-                                        />
-                                        <a
-                                          href={item.href}
-                                          className="mt-6 block font-medium text-gray-900"
-                                        >
-                                          <span
-                                            aria-hidden="true"
-                                            className="absolute inset-0 z-10"
-                                          />
-                                          {item.name}
-                                        </a>
-                                        <p aria-hidden="true" className="mt-1">
-                                          Shop now
-                                        </p>
-                                      </div>
-                                    ))}
-                                  </div>
-                                  <div className="row-start-1 grid grid-cols-3 gap-x-8 gap-y-10 text-sm">
-                                    {category.sections.map((section) => (
-                                      <div key={section.name}>
-                                        <p
-                                          id={`${section.name}-heading`}
-                                          className="font-medium text-gray-900"
-                                        >
-                                          {section.name}
-                                        </p>
-                                        <ul
-                                          role="list"
-                                          aria-labelledby={`${section.name}-heading`}
-                                          className="mt-6 space-y-6 sm:mt-4 sm:space-y-4"
-                                        >
-                                          {section.items.map((item) => (
-                                            <li key={item.name} className="flex">
-                                              <p
-                                                onClick={() => {
-                                                  handleCategoryClick(category, section, item);
-                                                  close(); // Close the popover
-                                                }}
-                                                className="cursor-pointer hover:text-gray-800"
-                                              >
-                                                {item.name}
-                                              </p>
-                                            </li>
-                                          ))}
-                                        </ul>
-                                      </div>
-                                    ))}
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                          </Popover.Panel>
-                        </>
-                      )}
-                    </Popover>
-                  ))}
-
-                  {navigation.pages.map((page) => (
-                    <a
-                      key={page.name}
-                      href={page.href}
-                      className="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800"
-                    >
-                      {page.name}
-                    </a>
-                  ))}
-                </div>
-              </PopoverGroup>
-
-
               <div className="ml-auto flex items-center">
                 <div className="flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
-                  {auth?.user?.firstName ? (
+                  {loading ? (
+                    <CircularProgress size={30} />
+                  ) : auth?.user?.firstName ? (
                     <div>
                       <Avatar
                         className="text-white"
                         onClick={handleUserClick}
-                        aria-controls={open ? "basic-menu" : undefined}
-                        aria-haspopup="true"
-                        aria-expanded={open ? "true" : undefined} // onClick={handleUserClick}
                         sx={{
                           bgcolor: deepPurple[500],
                           color: "white",
                           cursor: "pointer"
                         }}>
-                        { }
                       </Avatar>
 
                       <Menu
@@ -419,7 +123,7 @@ export default function Example() {
                           "aria-labelledby": "basic-button",
                         }}
                       >
-                        <MenuItem onClick={() => { navigate('/account/order') }}>My Orders</MenuItem>
+                        <MenuItem onClick={() => navigate('/account/order')}>My Orders</MenuItem>
                         <MenuItem onClick={handleLogout}>Logout</MenuItem>
                       </Menu>
                     </div>
@@ -427,14 +131,12 @@ export default function Example() {
                     <Button onClick={handleOpen} className="text-sm font-medium text-gray-700 hover:text-gray-800">
                       {location.pathname === "/login" || location.pathname === "/" ? 'Sign in' : location.pathname === "/verify" ? 'Verify Email' : 'Sign Up'}
                     </Button>
-                  )
-                  }
+                  )}
                 </div>
 
                 {/* Cart */}
                 <div className="ml-4 flow-root lg:ml-6">
-                  {
-                    jwt && auth?.user?.firstName?.[0]?.toUpperCase() &&
+                  {jwt && auth?.user?.firstName?.[0]?.toUpperCase() && (
                     <a href="/cart" className="group -m-2 flex items-center p-2">
                       <ShoppingBagIcon
                         aria-hidden="true"
@@ -442,7 +144,7 @@ export default function Example() {
                       />
                       <span className="sr-only">items in cart, view bag</span>
                     </a>
-                  }
+                  )}
                 </div>
               </div>
             </div>
@@ -451,6 +153,6 @@ export default function Example() {
       </header>
 
       <AuthModal handleClose={handleClose} open={openAuthModal} />
-    </div >
-  )
+    </div>
+  );
 }
